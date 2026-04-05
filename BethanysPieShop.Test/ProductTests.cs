65a252ece1d63292e2d0d08e80c465b83a651afd
Koustav Mandal <koustavmandal95@gmail.com>
2026-04-05 17:25:26 +0530

    addition of few more testcases

--- BethanysPieShop.Test/ProductTests.cs
+++ BethanysPieShop.Test/ProductTests.cs
@@ -10,7 +10,6 @@ public class ProductTests
             //Arrange
             Product product = new Product(1, "Sugar", "Lorem ipsum", new Price() { ItemPrice = 10, Currency = Currency.Euro }, UnitType.PerKg, 100);
 
-            Debug.WriteLine(product.AmountInStock);
             product.IncreaseStock(100);
 
             //Act
@@ -19,5 +18,31 @@ public class ProductTests
             //Assert
             Assert.Equal(80, product.AmountInStock);
 
+        }
+        [Fact]
+        public void UseProduct_ItemsHigherThanStock_NoChangetoStock()
+        {
+            //Arrange
+            Product product = new Product(1, "Sugar", "Lorem ipsum", new Price() { ItemPrice = 10, Currency = Currency.Euro }, UnitType.PerKg, 100);
+
+            product.IncreaseStock(10);
+
+            //Act
+            product.UseProduct(100);
+
+            //Assert
+            Assert.Equal(10, product.AmountInStock);
+        }
+                [Fact]
+        public void UseProduct_Reduces_AmountInStock_StockBelowTreshold()
+        {
+
+            Product product = new Product(1, "Sugar", "Lorem ipsum", new Price() { ItemPrice = 10, Currency = Currency.Euro }, UnitType.PerKg, 100);
+            int increaseValue = 100;
+            product.IncreaseStock(increaseValue);
+
+            product.UseProduct(increaseValue - 1);
+
+            Assert.False(product.IsBelowStockTreshold);
         }
 }
